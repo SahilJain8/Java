e58d36f26d8cd43b5a7bf2748655c07d90a1af79
Sahil <jainsahil1116@gmail.com>
2023-06-16 15:40:27 +0530

    methods

--- methods/src/Main.java
+++ methods/src/Main.java
@@ -1,12 +1,28 @@
 import  java.util.Scanner;
 
 public class Main {
+
+    static void print_num(){   //this is a fun that takes to numbers prints there sum
+        Scanner scanner = new Scanner(System.in);
+        int num1,num2,num;
+        System.out.println("Enter a number 1 ");
+        num1 = scanner.nextInt();
+
+        System.out.println("Enter a number 2 ");
+        num2 = scanner.nextInt();
+
+        num = num1 + num2;
+        System.out.println("The sum   is  "+num);
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Scanner scanner = new Scanner(System.in);
-        int num;
-        System.out.println("Enter a number");
-        num = scanner.nextInt();
-        System.out.println(num);
+        print_num();
+
     }
-}
+
+    //syntax of a method
+    // access modifier return type name() {
+    //body of the funtion
+//}
+
+    }
