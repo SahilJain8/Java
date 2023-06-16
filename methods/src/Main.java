c6b3a54d703a4fd5ce297e44c52cdd8ef6ca59db
Sahil <jainsahil1116@gmail.com>
2023-06-16 15:33:27 +0530

    methods

---
+++ methods/src/Main.java
@@ -0,0 +1,12 @@
+import  java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        System.out.println("Enter a number");
+        num = scanner.nextInt();
+        System.out.println(num);
+    }
+}
